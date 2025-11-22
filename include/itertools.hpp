21331e9685d8ad9f40d66d62cca1234a63cd2761
igorosky <igor.zaworski2@gmail.com>
2025-11-23 00:46:25 +0100

    One header
    Signed-off-by: igorosky <igor.zaworski2@gmail.com>

--- include/itertools.hpp
+++ include/itertools.hpp
@@ -2,10 +2,51 @@
 
 #include <optional>
 #include <type_traits>
+#include <unordered_set>
 #include <utility>
-#include "defer.hpp"
 
 namespace itertools {
+
+// Internal utility to implement defer semantics
+namespace {
+template<typename Fn>
+class Defer {
+  static_assert(std::is_invocable_v<Fn>, "Defer requires an invocable type");
+  static_assert(std::is_same_v<std::invoke_result_t<Fn>, void>,
+                "Defer function is required to return a void type");
+  Fn _fn;
+
+  // Disable copy and move semantics
+  Defer(const Defer&) = delete;
+  Defer& operator=(const Defer&) = delete;
+  Defer(Defer&&) = delete;
+  Defer& operator=(Defer&&) = delete;
+
+public:
+  [[nodiscard]] inline explicit Defer(Fn&& fn) noexcept(std::is_nothrow_move_constructible_v<Fn>)
+  : _fn(std::move(fn)) { }
+
+  inline ~Defer() noexcept(std::is_nothrow_invocable_v<Fn>) {
+    _fn();
+  }
+};
+}  // namespace
+
+#ifdef __COUNTER__
+#  define IT_COUNTER __COUNTER__
+#else
+#  define IT_COUNTER __LINE__
+#endif
+
+#define IT_CONCAT_HELPER(a, b) a ## b
+#define IT_CONCAT(a, b) IT_CONCAT_HELPER(a, b)
+#define IT_UNIQUE_NAME(prefix) IT_CONCAT(prefix, IT_COUNTER)
+#define IT_DEFER(fn) \
+  itertools::Defer IT_UNIQUE_NAME(it_defer)(fn);
+
+
+// Iterable implementation
+
 template <typename T, typename Generator>
 class Iterable {
 public:
@@ -110,6 +151,10 @@ public:
   }
 };
 
+
+// Utility transformations
+
+
 template <typename Iter>
 auto iter(const Iter& iter) {
   using prev_iter = decltype(std::declval<Iter>().begin());
@@ -199,4 +244,159 @@ auto iter(Iter begin, Iter end) {
     { std::move(begin), std::move(end) }
   };
 }
+
+
+// Transformation implementations
+
+
+struct Count {
+  template <typename T, typename Iter>
+  auto operator()(itertools::Iterable<T, Iter> iter) const {
+    size_t count = 0;
+    auto it = iter.begin();
+    while (it.next().has_value()) {
+      ++count;
+    }
+    return count;
+  }
+};
+
+struct Sum {
+  template <typename T, typename Iter>
+  auto operator()(itertools::Iterable<T, Iter> iter) const {
+    std::remove_reference_t<typename itertools::Iterable<T, Iter>::Iterator::value_type> total = 0;
+    auto it = iter.begin();
+    while (auto val = it.next()) {
+      total += it.get_val(std::move(val));
+    }
+    return total;
+  }
+};
+
+struct Map {
+  template <typename T, typename Iter, typename Func>
+  auto operator()(itertools::Iterable<T, Iter> it, Func&& func) const {
+    using new_value_type = std::invoke_result_t<Func, decltype(*it.begin())>;
+    static_assert(!std::is_reference_v<new_value_type>,
+      "Mapping function must not return a reference type");
+
+    struct Mapper {
+      decltype(it.begin()) iter;
+      std::remove_reference_t<Func> func;
+
+      auto operator()() {
+        auto next_val = iter.next();
+        if (next_val.has_value()) {
+          return std::optional{ func(*next_val.value()) };
+        }
+        return std::optional<typename std::invoke_result_t<Func, std::remove_pointer_t<typename itertools::Iterable<T, Iter>::Iterator::value_type>>>{ };
+      }
+    };
+    return itertools::Iterable<typename std::invoke_result_t<Mapper>::value_type, Mapper>{
+      { it.begin(), std::move(func) }
+    };
+  }
+};
+
+struct Filter {
+  template <typename T, typename Iter, typename Func>
+  auto operator()(itertools::Iterable<T, Iter> it, Func&& func) const {
+    struct Filterer {
+      decltype(it.begin()) iter;
+      std::remove_reference_t<Func> func;
+
+      auto operator()() {
+        auto next_val = iter.next();
+        while (next_val.has_value() && !func(iter.get_val(std::move(next_val)))) {
+          next_val = iter.next();
+        }
+        return next_val;
+      }
+    };
+    return itertools::Iterable<T, Filterer>{
+      { std::move(it.begin()), std::move(func) }
+    };
+  }
+};
+
+template <typename Target>
+struct Collect {
+  template <typename T, typename Iter>
+  auto operator()(itertools::Iterable<T, Iter> iter) const {
+    return Target(iter.begin(), iter.end());
+  }
+};
+
+struct ForEach {
+  template <typename T, typename Iter, typename Func>
+  void operator()(itertools::Iterable<T, Iter> iter, Func&& func) const {
+    auto it = iter.begin();
+    while (auto val = it.next()) {
+      func(it.get_val(std::move(val)));
+    }
+  }
+};
+
+struct Zip {
+  template <typename T, typename Iter, typename Y, typename OtherIter>
+  auto operator()(itertools::Iterable<T, Iter> iter, itertools::Iterable<Y, OtherIter> other) const {
+    using iter_type = decltype(iter.begin());
+    using other_type = decltype(other.begin());
+    using pair_type = std::pair<decltype(*std::declval<iter_type>()), decltype(*std::declval<other_type>())>;
+    struct Zipper {
+      iter_type iter1;
+      other_type iter2;
+      auto operator()() {
+        auto val1 = iter1.next();
+        auto val2 = iter2.next();
+        if (val1.has_value() && val2.has_value()) {
+          return std::optional<pair_type>{
+            { iter1.get_val(std::move(val1)), iter2.get_val(std::move(val2)) }
+          };
+        }
+        return std::optional<pair_type>{ };
+      }
+    };
+    return itertools::Iterable<pair_type, Zipper>{
+      { iter.begin(), other.begin() }
+    };
+  }
+};
+
+struct Unique {
+  template <typename T, typename Iter>
+  auto operator()(itertools::Iterable<T, Iter> iter) const {
+    using iter_type = decltype(iter.begin());
+
+    struct Uniquifier {
+      iter_type iter;
+      std::unordered_set<std::remove_reference_t<T>> seen_values;
+
+      auto operator()() {
+        auto next_val = iter.next();
+        while (next_val.has_value()) {
+          auto val = iter.get_val(std::move(next_val));
+          if (seen_values.insert(val).second) {
+            return std::optional<T>{ val };
+          }
+          next_val = iter.next();
+        }
+        return std::optional<T>{ };
+      }
+    };
+    return itertools::Iterable<T, Uniquifier>{
+      { iter.begin(), { } }
+    };
+  }
+};
+
+
 }  // namespace itertools
+
+
+// Cleanup internal macros
+#undef IT_COUNTER
+#undef IT_CONCAT_HELPER
+#undef IT_CONCAT
+#undef IT_UNIQUE_NAME
+#undef IT_DEFER
--- src/main.cpp
+++ src/main.cpp
@@ -1,150 +1,16 @@
 #include <iostream>
-#include <unordered_set>
 #include <vector>
 #include "itertools.hpp"
 
 using itertools::iter;
-
-struct Count {
-  template <typename T, typename Iter>
-  auto operator()(itertools::Iterable<T, Iter> iter) const {
-    size_t count = 0;
-    auto it = iter.begin();
-    while (it.next().has_value()) {
-      ++count;
-    }
-    return count;
-  }
-};
-
-struct Sum {
-  template <typename T, typename Iter>
-  auto operator()(itertools::Iterable<T, Iter> iter) const {
-    std::remove_reference_t<typename itertools::Iterable<T, Iter>::Iterator::value_type> total = 0;
-    auto it = iter.begin();
-    while (auto val = it.next()) {
-      total += it.get_val(std::move(val));
-    }
-    return total;
-  }
-};
-
-struct Map {
-  template <typename T, typename Iter, typename Func>
-  auto operator()(itertools::Iterable<T, Iter> it, Func&& func) const {
-    using new_value_type = std::invoke_result_t<Func, decltype(*it.begin())>;
-    static_assert(!std::is_reference_v<new_value_type>,
-      "Mapping function must not return a reference type");
-
-    struct Mapper {
-      decltype(it.begin()) iter;
-      std::remove_reference_t<Func> func;
-
-      auto operator()() {
-        auto next_val = iter.next();
-        if (next_val.has_value()) {
-          return std::optional{ func(*next_val.value()) };
-        }
-        return std::optional<typename std::invoke_result_t<Func, std::remove_pointer_t<typename itertools::Iterable<T, Iter>::Iterator::value_type>>>{ };
-      }
-    };
-    return itertools::Iterable<typename std::invoke_result_t<Mapper>::value_type, Mapper>{
-      { it.begin(), std::move(func) }
-    };
-  }
-};
-
-struct Filter {
-  template <typename T, typename Iter, typename Func>
-  auto operator()(itertools::Iterable<T, Iter> it, Func&& func) const {
-    struct Filterer {
-      decltype(it.begin()) iter;
-      std::remove_reference_t<Func> func;
-
-      auto operator()() {
-        auto next_val = iter.next();
-        while (next_val.has_value() && !func(iter.get_val(std::move(next_val)))) {
-          next_val = iter.next();
-        }
-        return next_val;
-      }
-    };
-    return itertools::Iterable<T, Filterer>{
-      { std::move(it.begin()), std::move(func) }
-    };
-  }
-};
-
-template <typename Target>
-struct Collect {
-  template <typename T, typename Iter>
-  auto operator()(itertools::Iterable<T, Iter> iter) const {
-    return Target(iter.begin(), iter.end());
-  }
-};
-
-struct ForEach {
-  template <typename T, typename Iter, typename Func>
-  void operator()(itertools::Iterable<T, Iter> iter, Func&& func) const {
-    auto it = iter.begin();
-    while (auto val = it.next()) {
-      func(it.get_val(std::move(val)));
-    }
-  }
-};
-
-struct Zip {
-  template <typename T, typename Iter, typename Y, typename OtherIter>
-  auto operator()(itertools::Iterable<T, Iter> iter, itertools::Iterable<Y, OtherIter> other) const {
-    using iter_type = decltype(iter.begin());
-    using other_type = decltype(other.begin());
-    using pair_type = std::pair<decltype(*std::declval<iter_type>()), decltype(*std::declval<other_type>())>;
-    struct Zipper {
-      iter_type iter1;
-      other_type iter2;
-      auto operator()() {
-        auto val1 = iter1.next();
-        auto val2 = iter2.next();
-        if (val1.has_value() && val2.has_value()) {
-          return std::optional<pair_type>{
-            { iter1.get_val(std::move(val1)), iter2.get_val(std::move(val2)) }
-          };
-        }
-        return std::optional<pair_type>{ };
-      }
-    };
-    return itertools::Iterable<pair_type, Zipper>{
-      { iter.begin(), other.begin() }
-    };
-  }
-};
-
-struct Unique {
-  template <typename T, typename Iter>
-  auto operator()(itertools::Iterable<T, Iter> iter) const {
-    using iter_type = decltype(iter.begin());
-
-    struct Uniquifier {
-      iter_type iter;
-      std::unordered_set<std::remove_reference_t<T>> seen_values;
-
-      auto operator()() {
-        auto next_val = iter.next();
-        while (next_val.has_value()) {
-          auto val = iter.get_val(std::move(next_val));
-          if (seen_values.insert(val).second) {
-            return std::optional<T>{ val };
-          }
-          next_val = iter.next();
-        }
-        return std::optional<T>{ };
-      }
-    };
-    return itertools::Iterable<T, Uniquifier>{
-      { iter.begin(), { } }
-    };
-  }
-};
+using itertools::Count;
+using itertools::Sum;
+using itertools::Map;
+using itertools::Filter;
+using itertools::Unique;
+using itertools::ForEach;
+using itertools::Zip;
+using itertools::Collect;
 
 int main() {
   std::vector<int> vec = {1, 2, 3, 4, 5};
